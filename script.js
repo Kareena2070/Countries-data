a2ada842fe85df8d0ee80d6ea624732a3c5fa9b4
Kareena2070 <kareenayadav1088@gmail.com>
2025-07-11 23:13:03 +0530

    today I remove all the html and css, made this project just using JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,137 @@
-const container = document.getElementById("countries-container");
-const searchInput = document.getElementById("search");
+
+let body = document.body
+let style = document.createElement("style");
+style.innerHTML = `
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background: #f0f0f0;
+    cursor: pointer;
+  }
+
+    .navbar {
+        background: rgb(229, 224, 224);
+        height: 8em;
+        width: 100%;
+        text-align: center;
+        padding: 1em;
+        font-family: Arial, sans-serif;
+    }
+    .navbar h1{
+        letter-spacing: 0.1em;
+        color: orange;
+        font-size: 3em;
+        margin-top: 0.5em;
+        }
+
+    #searchInput{
+    width: 50%;
+    height: 2em;
+    border-radius: 1em;
+    padding: 0.5em;
+    font-size: 1em;
+    margin-top: 2rem;
+    border: none;
+    
+  }
+
+button {
+    background-color: #f2882b; 
+    border: none;
+    padding: 10px 20px;
+    font-size: 1.2em;
+    margin-top: 1em;
+    border-radius: 1em;
+    margin-left: 1em;
+  }
+
+button:hover {
+    background-color: #f9a53e; 
+  }
+i{
+  font-size: 3em;
+  color: #f2882b;
+  margin-left: 0.5em;
+}
+
+#countries-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1.5em;
+    margin-top: 2em;
+  }
+
+
+
+#result-msg{
+  color: #f2882b;
+  margin-top: 0.5em;
+}
+
+#chart {
+  width: 80%;
+  margin: 20px auto;
+}
+.bar {
+  background-color: skyblue;
+  margin: 5px 0;
+  padding: 10px;
+  color: black;
+  font-weight: bold;
+}
+
+.div3{
+  background-color: aqua;
+}
+`;
+document.head.appendChild(style);
+
+let navDiv = document.createElement("div")
+navDiv.className = "navbar"
+navDiv.innerHTML =`
+<h1>World Countries Data</h1>
+<p> Currently, we have 250 Countries</p>
+
+`;
+
+body.appendChild(navDiv)
+
+resultMsg = document.createElement("p")
+resultMsg.id = "result-msg"
+navDiv.append(resultMsg)
+
+
+
+let searchContainer = document.createElement("div")
+searchContainer.id = "search-container"
+searchContainer.innerHTML =`
+        <div>
+            <input type="text" id="searchInput" placeholder="Search by name, capital or language">
+        </div>
+        <div class="buttons">
+            <button id="name">NAME</button>
+            <button id="capital">CAPITAL</button>
+            <button id="population">POPULATION</button>
+            <i id="graph" class="fa-solid fa-chart-simple"></i>
+        </div>
+    `;
+body.appendChild(searchContainer)
+
+
+let countriesContainer = document.createElement("div")
+countriesContainer.id = "countries-container"
+body.appendChild(countriesContainer)
+
 
 // Function to create country cards
 function displayCountries(countries) {
-  container.innerHTML = ""; 
+  countriesContainer.innerHTML = ""; 
   countries.forEach(country => {
     const div = document.createElement("div");
     div.style.background = "white";
@@ -18,14 +146,14 @@ function displayCountries(countries) {
       <p><strong>Region:</strong> ${country.region}</p>
       <p><strong>Area:</strong> ${country.area}</p>
     `;
-    container.appendChild(div);
+    countriesContainer.appendChild(div);
   });
 }
 
 // calling the display function with the initial data
 displayCountries(countries);
 
-const resultMsg = document.getElementById("result-msg");
+// show numbers of countries according to the user search
 
 searchInput.addEventListener("input", () => {
   const keyword = searchInput.value.toLowerCase();
@@ -42,7 +170,6 @@ searchInput.addEventListener("input", () => {
 const name1 = document.getElementById("name")
 const capital = document.getElementById("capital")
 const population = document.getElementById("population")
-// const graph = document.getElementById("graph")
 
 // each button are click able 
 
@@ -93,7 +220,7 @@ graph.addEventListener("click", ()=>{
   });
 })
 
-
+// Bar graph 
 
 let chartDiv = document.createElement("div")
 chartDiv.style.paddingLeft = "20em"
@@ -101,7 +228,7 @@ chartDiv.style.paddingRight = "21em"
 chartDiv.style.paddingTop = "4em"
 chartDiv.style.paddingBottom ="2em"
 chartDiv.style.backgroundColor = "#fefae0"
-container.after(chartDiv)
+countriesContainer.after(chartDiv)
 
 let canvas = document.createElement("canvas")
 canvas.id = "myChart"
